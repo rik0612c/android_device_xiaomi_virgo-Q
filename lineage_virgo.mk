dec9fe5f6854a663bc83729adbfa090b6d9a0bc1
rik0612c <qq872638399@gmail.com>
2023-01-29 00:16:24 +0800

    lineage-ify

--- lineage_virgo.mk
+++ lineage_virgo.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/virgo/virgo.mk)
 
 # Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mokee/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := mokee_virgo
+PRODUCT_NAME := lineage_virgo
 PRODUCT_DEVICE := virgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
